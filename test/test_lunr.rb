aa582a14a9b4a5f067915162d50b85a126fb1d4e
John Barnette <jbarnette@gmail.com>
2010-08-25 02:47:41 -0700

    Stub test harness.

--- test/test_lunr.rb
+++ test/test_lunr.rb
@@ -1,8 +1,25 @@
 require "minitest/autorun"
+require "fakeweb"
 require "lunr"
 
+FakeWeb.allow_net_connect = false
+
 class TestLunr < MiniTest::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+  def setup
+    FakeWeb.clean_registry
+  end
+
+  def test_self_search
+    stub "foo"
+    Lunr.search "foo"
+  end
+
+  def stub query, fixture = "simple"
+    FakeWeb.register_uri :get, url(query),
+      :body => File.read("test/fixtures/#{fixture}")
+  end
+
+  def url query
+    "#{Lunr[:url]}/select?wt=ruby&start=0&q=#{query}&rows=25"
   end
 end
